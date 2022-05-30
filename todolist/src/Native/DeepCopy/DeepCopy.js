e9ff46e3b53fd48afdb56422a3a2e1d23add3547
DmitryPatskalev <dimanord@yandex.ru>
2022-05-30 14:30:24 +0300

    commit

---
+++ todolist/src/Native/DeepCopy/DeepCopy.js
@@ -0,0 +1,36 @@
+let obj = {
+    name:'Dima',
+    age:37
+}
+let obj2 = {...obj}
+
+obj2.name = "Danik"
+
+
+let person = {
+    name:'Dima',
+    age:37,
+    adress:{
+        city:'Minsk',
+        street:{
+            street: 'Lazo'
+        }
+    }
+}
+let person2 = {...person, age:7, name:'Danik', adress:{...person.adress, ...person.adress.street, street:{street:'Striy'}}}
+console.log(person)
+console.log(person2)
+
+let arr = [
+    {name:'Dima', age:37},
+    {name:'Danik', age:7}
+]
+
+let newArr = arr.map(elem=>({...elem}))
+newArr[0].name = 'Tania'
+newArr[0].age  = 31
+console.log(arr)
+console.log(newArr)
+
+let str = JSON.parse(JSON.stringify(arr))
+console.log(str)
